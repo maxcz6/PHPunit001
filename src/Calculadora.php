e36aab0a522220d961d091759bef51b26708707a
maxcz6 <maxjpr7@gmail.com>
2026-06-01 13:59:27 -0500

    vercion 2

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -1,8 +1,42 @@
 <?php
-
+# ejecutar vendor/bin/phpunit test
+//semana 8
 namespace App;
 class Calculadora {
     public function sumar($a, $b) {
         return $a + $b;
     }
+
+    public function restar($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiplicar($a, $b) {
+        return $a * $b;
+    }
+
+    public function dividir($a, $b) {
+        if ($b == 0) {
+            throw new \InvalidArgumentException("Divicion entre cero");
+        }
+        return $a / $b;
+    }
+    public function esPar($numero) {
+        return $numero % 2 === 0;
+    }
+
+    public function esPositivo($numero) {
+        return $numero > 0;
+    }
+
+    public function esNegativo($numero) {
+        return $numero < 0;
+    }
+    public function esCero($numero) {
+        return $numero === 0;
+    }
+
+
+
+
 }
--- test/calculadoraTest.php
+++ test/calculadoraTest.php
@@ -1,5 +1,5 @@
 <?php
-
+# ejecutar vendor/bin/phpunit test
 use PHPUnit\Framework\TestCase;
 use App\Calculadora;
 
@@ -16,4 +16,34 @@ class CalculadoraTest extends TestCase {
         $resultado = $calculadora->sumar(2, 3);
         $this->assertEquals(5, $resultado);
     }
+    // -> empieza la semana 8 
+    //ejercicio 1
+    public function testEsPar() {
+        $calculadora = new Calculadora();
+        $this->assertTrue($calculadora->esPar(4));
+        $this->assertFalse($calculadora->esPar(5));
+        $this->assertTrue($calculadora->esPar(0));
+    }
+    //ejercicio 2
+    public function testEsPositivo() {
+        $calculadora = new Calculadora();
+        $this->assertTrue($calculadora->esPositivo(10));
+        $this->assertFalse($calculadora->esPositivo(-5));
+        $this->assertFalse($calculadora->esPositivo(0));
+    }
+    //ejercicio 3
+    public function testEsNegativo() {
+        $calculadora = new Calculadora();
+        $this->assertTrue($calculadora->esNegativo(-10));
+        $this->assertFalse($calculadora->esNegativo(5));
+        $this->assertFalse($calculadora->esNegativo(0));
+    }
+    //ejercicio 4
+    public function testEsCero() {
+        $calculadora = new Calculadora();
+        $this->assertTrue($calculadora->esCero(0));
+        $this->assertFalse($calculadora->esCero(5));
+        $this->assertFalse($calculadora->esCero(-5));
+    }
+
 }
